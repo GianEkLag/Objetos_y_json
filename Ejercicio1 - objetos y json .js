1dea9fb7ef3aef63b74f06bcb6f086fec36013f5
GianEkLag <78385779+GianEkLag@users.noreply.github.com>
2021-11-29 23:32:16 -0500

    Add files via upload

---
+++ Ejercicio1 - objetos y json .js
@@ -0,0 +1,114 @@
+var object1 = {
+    menú: {
+        encabezado: "Visor de SVG",
+        elementos: [
+            {
+                id: "Abierto"
+            },
+            {
+                id: "OpenNew",
+                label: "Abrir nuevo"
+            },
+            nulo,
+            {
+                id: "ZoomIn",
+                etiqueta: "Acercar"
+            },
+            {
+                id: "ZoomOut",
+                etiqueta: "Alejar"
+            },
+            {
+                id: "OriginalView",
+                etiqueta: "Vista original"
+            },
+            nulo,
+            {
+                id: "Calidad"
+            },
+            {
+                id: "Pausa"
+            },
+            {
+                id: "Mute"
+            },
+            nulo,
+            {
+                id: "Buscar",
+                etiqueta: "Buscar ..."
+            },
+            {
+                id: "FindAgain",
+                label: "Buscar de nuevo"
+            },
+            {
+                id: "Copiar"
+            },
+            {
+                id: "CopyAgain",
+                label: "Copiar de nuevo"
+            },
+            {
+                id: "CopySVG",
+                etiqueta: "Copiar SVG"
+            },
+            {
+                id: "ViewSVG",
+                etiqueta: "Ver SVG"
+            },
+            {
+                id: "ViewSource",
+                etiqueta: "Ver código fuente"
+            },
+            {
+                id: "Guardar como",
+                etiqueta: "Guardar como"
+            },
+            nulo,
+            {
+                id: "Ayuda"
+            },
+            {
+                id: "Acerca de",
+                label: "Acerca de Adobe CVG Viewer ..."
+            }
+        ]
+    }
+};
+
+var object2 = {
+    id: "ViewSVG",
+    etiqueta: "Ver SVG"
+};
+
+var object3 = {
+    id: "ViewSource",
+    etiqueta: "Ver código fuente"
+};
+
+var object4 = {
+    id: "Guardar como",
+    etiqueta: "Guardar como"
+};
+
+var object5 = {
+    id: "Guardar como",
+    etiqueta: "Guardar como"
+};
+
+var object6 = {
+    id: "Ayuda"
+};
+
+var object7 = {
+    id: "Ayuda"
+};
+
+var object8 = {
+    id: "Ayuda"
+};
+
+var object9 = {
+    id: "Acerca de",
+    label: "Acerca de Adobe CVG Viewer ..."
+};
